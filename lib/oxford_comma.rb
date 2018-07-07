4906a2ebe889c56d56acc11f8a41de5d730f9603
Juan Tenezaca <tenj34@gmail.com>
2018-07-07 19:09:31 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,9 +5,9 @@ if count  == 1
   return array.join
 elsif count == 2
   array.join(" and ")
-elsif count == 3
-  array.insert(-2, "and ")
-
+else
+  array[-1].prepend "and"
+  array.join(", ")
 
 end
 
